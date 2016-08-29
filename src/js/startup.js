10cd103371f8c4d6845fd32a193e116d23d6d3c7
nazikus <nazik.g@gmail.com>
2016-08-30 01:22:28 +0300

    d3 initial steps

--- src/js/startup.js
+++ src/js/startup.js
@@ -1,4 +1,14 @@
-var __jsonUrl__ = 'https://script.google.com/macros/s/AKfycbw2S0RPmxM8oT8uXwY8NmG6WoHEljbx1XXDhUU2v3QYQQqRL7LO/exec';
+// timeline params
+var tl = {
+    dataUrl: 'https://script.google.com/macros/s/AKfycbw2S0RPmxM8oT8uXwY8NmG6WoHEljbx1XXDhUU2v3QYQQqRL7LO/exec',
+    
+    EVENTS_CANVAS_ID: '#events-canvas',
+    WORKS_CANVAS_ID: '#works-canvas',
 
-timelineApp.loadTimelineData(__jsonUrl__); // async
-timelineApp.initTimelines(); // waits for async
+    margin: { top: 20, right: 20, bottom: 30, left: 40 }
+}
+
+
+d3.json(tl.dataUrl, function(error, data){
+    if (error) throw error;
+})
